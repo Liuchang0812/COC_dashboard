2d4bc0219ba27deb37b3f5f833db5b5e26b857e9
iriyadays <iriyadays@gmail.com>
2015-05-22 15:42:16 +0800

    add api test

---
+++ tests/apitest.py
@@ -0,0 +1,20 @@
+import unittest
+import sys
+import json
+
+sys.path.append("../api")
+from api import *
+
+class apitest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_clan_by_name(self):
+        resp = clan_by_name('E7VMK')
+	self.assertEquals('E7VMK7UK', resp["Name"])
+
+if __name__ == '__main__':
+    unittest.main()
